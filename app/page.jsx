ada8b93c73bdc0e05e242fa7b495fbe030fd31fa
sayanbanik08 <sayanbanik08@github.com>
2025-09-18 00:31:59 +0530

    save

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
 import Banner from "@/components/Banner";
-import NewsLetter from "@/components/NewsLetter";
-import FeaturedProduct from "@/components/FeaturedProduct";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -15,9 +13,7 @@ const Home = () => {
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <HomeProducts />
-        <FeaturedProduct />
         <Banner />
-        <NewsLetter />
       </div>
       <Footer />
     </>
--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -4,13 +4,9 @@ const NewsLetter = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center space-y-2 pt-8 pb-14">
       <h1 className="md:text-4xl text-2xl font-medium">
-        Subscribe now & get 20% off
+        Newsletter
       </h1>
-      <p className="md:text-base text-gray-500/80 pb-8">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
-      </p>
-      <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
+      <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12 mt-8">
         <input
           className="border border-gray-500/30 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500"
           type="text"
